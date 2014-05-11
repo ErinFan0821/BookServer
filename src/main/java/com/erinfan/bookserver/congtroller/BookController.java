b53f63d40251d98c8c0878a16720a2db5f82e12d
techops <fanxiao1987821@gmail.com>
2014-05-11 12:02:27 +0800

    config book controller

---
+++ src/main/java/com/erinfan/bookserver/congtroller/BookController.java
@@ -0,0 +1,20 @@
+package com.erinfan.bookserver.congtroller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by techops on 5/11/14.
+ */
+
+public class BookController extends AbstractController {
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView view = new ModelAndView("index");
+        view.addObject("msg", "hello spring");
+        return view;
+    }
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1> Book Server</h1>
+<h1>${msg}</h1>
+</body>
+</html>
